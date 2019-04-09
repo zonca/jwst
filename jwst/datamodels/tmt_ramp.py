48ec2e49046de68f0a16db74959685ed3b44690e
Andrea Zonca <code@andreazonca.com>
2019-04-09 12:47:33 -0700

    added ramp model for TMT

---
+++ jwst/datamodels/tmt_ramp.py
@@ -0,0 +1,43 @@
+from .model_base import DataModel
+
+
+__all__ = ['TMTRampModel']
+
+
+class TMTRampModel(DataModel):
+    """
+    A data model for 4D ramps.
+
+    Parameters
+    __________
+    data : numpy float32 array
+         The science data
+
+    pixeldq : numpy uint32 array
+         2-D data quality array for all planes
+
+    groupdq : numpy uint8 array
+         4-D data quality array for each plane
+
+    err : numpy float32 array
+         Error array
+
+    zeroframe : numpy float32 array
+         Zeroframe array
+
+    group : numpy table
+         group parameters table
+
+    int_times : numpy table
+         table of times for each integration
+
+    """
+    schema_url = "tmt_ramp.schema.yaml"
+
+    def __init__(self, init=None, **kwargs):
+        super().__init__(init=init, **kwargs)
+
+        # Implicitly create arrays
+        self.pixeldq = self.pixeldq
+        self.groupdq = self.groupdq
+        self.err = self.err
